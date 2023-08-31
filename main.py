00de7283d2d718a435cebc6d66b470eab89b7196
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-08-31 22:10:52 +0200

    up

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ import os
 import matplotlib.pyplot as plt
 
 # Set your OpenAI API key
-openai.api_key = os.getenv("OPENAI_KEY")
+openai.api_key = st.secrets["OPENAI_KEY"]
 
-# Title and description
-st.title("Data Analysis App")
-st.write("Upload a CSV or Excel file for analysis.")
 
-# File upload
-uploaded_file = st.file_uploader("Upload a file", type=["csv", "xlsx"])
 
+# File upload in the sidebar
+st.sidebar.write("Upload a CSV or Excel file for analysis.")
+uploaded_file = st.sidebar.file_uploader("Upload a file", type=["csv", "xlsx"])
 
 if uploaded_file:
     # Load data
@@ -22,33 +20,12 @@ if uploaded_file:
     else:
         data = pd.read_csv(uploaded_file)
 
-    # Display the raw data
-    st.subheader("Raw Data")
-    st.write(data)
+    # Display the first 10 entries of the data in the sidebar
+    st.sidebar.subheader("First 10 Entries of Data")
+    st.sidebar.write(data.head(10))
 
-    # User input for analysis request
-    analysis_request = "Write a working python code that creates a matplot figure object and plot it with st.pyplot(fig). Only write the python code and nothing else for the following data: " + data.to_string(index=False) + "Only write the python code and nothing else."
 
-    if st.button("Generate Analysis Code"):
-        # Create a conversation with a system message and user message
-        conversation = [
-            {"role": "system", "content": "You are a data analysis expert."},
-            {"role": "user", "content": analysis_request}
-        ]
+# Title and description
+st.title("Data Analysis App")
 
-        # Send the conversation to the GPT-3 API to generate analysis code
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=conversation
-        )
 
-        # Extract and display the analysis code
-        analysis_code = response.choices[0].message["content"]
-        st.subheader("Generated Analysis Code:")
-        st.code(analysis_code, language="python")
-
-        # Execute the analysis code
-        try:
-            exec(analysis_code)
-        except Exception as e:
-            st.error(f"Error executing analysis code: {str(e)}")
