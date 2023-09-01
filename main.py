dfbd1dfdfe388e177352314f5e0c7398b92cf85d
robynvasco <113433151+robynvasco@users.noreply.github.com>
2023-09-01 10:11:15 +0200

    up

--- main.py
+++ main.py
@@ -84,7 +84,7 @@ if prompt := st.chat_input("Send a message"):
 
         for response in openai.ChatCompletion.create(
             model=st.session_state["openai_model"],
-            messages=conversation,
+            messages=[{"role": m["role"], "content": m["content"]} for m in st.session_state.messages[-3:]],
             stream=True,
         ):
             full_response += response.choices[0].delta.get("content", "")
